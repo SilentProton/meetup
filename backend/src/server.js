ac65fe3ad6bb61cae336d84c30b8316fcf7c098d
SilentProton <silentproton.25@gmail.com>
2025-07-20 16:41:40 +0530

    chore: set up MongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import 'dotenv/config';
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,4 +11,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    connectDB();
 });
