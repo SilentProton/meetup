b2f1a3203b7224dc5894c1ddb3509a30347f66e4
SilentProton <silentproton.25@gmail.com>
2025-07-20 10:10:56 +0530

    feat: add auth routes (login, logout, signup)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,13 @@
-import 'dotenv/config'
+import express from "express";
+import 'dotenv/config';
 
-const port = process.env.PORT
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
