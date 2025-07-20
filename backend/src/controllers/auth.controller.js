006abfc50d5359adf7ed0286201737d241f15e01
SilentProton <silentproton.25@gmail.com>
2025-07-20 20:42:06 +0530

    feat: implement user signup with validation and JWT auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,55 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken"
+
 export async function signup(req,res) {
-    res.send("Signup Route");
+    const {email, password, fullName} = req.body;
+
+    try {
+        if(!email || !password || !fullName){
+            return res.status(400).json({massage: "All fields are required"});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        if(password.length<6){
+            return res.status(400).json({massage: "Password must be at least 6 characters"});
+        }
+
+        const existinguser = await User.findOne({email});
+        if(existinguser){
+            return res.status(400).json({massage: "Email already exists, please use a different one."});
+        }
+
+        const idx = Math.floor(Math.random() * 100) + 1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
+        
+        const newUser = await User.create({
+            email,
+            fullName,
+            password,
+            profilePic: randomAvatar,
+        })
+
+        const token = jwt.sign({userId: newUser._id},process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d"
+        })
+
+        res.cookie("jwt",token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.status(201).json({success: true, user: newUser})
+
+    } catch (error) {
+        console.log("Error in Signup controller", error);
+        res.status(500).json({message: "Internal Server Error"})
+    }
 }
 
 export async function login(req,res) {
